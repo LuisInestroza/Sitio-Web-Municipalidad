955998bc363761be0951955a11bbbb277a041b11
Luis Dario <luisda13@yahoo.es>
2020-02-10 08:17:09 -0600

    Incorparación de elementos de imagenes en el sitio.

--- index.php
+++ index.php
@@ -3,25 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Municipalidad de Siguatepeque</title>
 </head>
 <body>
-    <!-- Logo -->
-    <div class="logo">
-        <img src="" alt="">
-    </div>
-
-    <!-- Navegación -->
-    <nav>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-    </nav>
-
-
-
-
+   
 </body>
 </html>
