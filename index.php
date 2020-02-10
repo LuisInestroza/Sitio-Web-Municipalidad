527bcdb75d7322793ad4c06020b95d94b29c1e0b
Luis Dario <luisda13@yahoo.es>
2020-02-10 10:45:45 -0600

    Creacion de barra de navegación del sitio con posibilidad de cambiar la imagen del banner

--- index.php
+++ index.php
@@ -3,21 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link de las hojas de estilos -->
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
+
+    <!-- Link de el estilo de las fuentes -->
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Links de iconos -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+</head>
     <title>Municipalidad de Siguatepeque</title>
 </head>
 <body>
 
 <div class="navegacion">
+    <!-- Banner -->
     <img class="banner" src="img/banner2.png" alt="">
+    <!-- Logo -->
     <img class= "logo"src="img/logoNuevo.png" alt="" srcset="">
-    <input class="buscar" type="search" name="" id=""  placeholder="Buscar">
+    <!-- Input de buscar -->
+    <input class="buscar" type="search" name="" id=""  placeholder="Buscar" >
+    
+    
     <nav class="navegacion">
-        
+        <a href="">Inicio</a>
+        <a href="">Noticias</a>
+        <a href="">Gobierno</a>
+        <a href="">Portal de Transparencia</a>
+        <a href="">Conoce a Siguatepeque</a>
+        <a href="">Login Linea Base</a>
+
     </nav>
+    <p class="letras">BIENVENIDOS A NUESTRO SITIO WEB</p>
+  
 </div>
+
+
    
 </body>
 </html>
