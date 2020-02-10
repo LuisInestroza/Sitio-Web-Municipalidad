4694cb92b41ae16803a8e92e3a378549909c6ecb
Luis Dario <luisda13@yahoo.es>
2020-02-10 09:10:39 -0600

    Implementación de las imagenes y comienzo de los estilos CSS

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <title>Municipalidad de Siguatepeque</title>
 </head>
 <body>
+
+<div class="navegacion">
+    <img class="banner" src="img/banner2.png" alt="">
+    <input type="search" name="" id="">
+    <nav class="navegacion">
+    </nav>
+</div>
    
 </body>
 </html>
