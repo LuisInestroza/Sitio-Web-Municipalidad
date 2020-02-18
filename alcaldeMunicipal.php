3a75835c4b93fd6345caadbe558ea2dcfdee2b85
Luis Dario <luisda13@yahoo.es>
2020-02-18 14:31:23 -0600

    Implementación de estilos de la view alcaldeMunicipal.php

--- alcaldeMunicipal.php
+++ alcaldeMunicipal.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Aceder al css -->
     <link rel="stylesheet" href="css/style.css">
-   
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <!-- Link de el estilo de las fuentes -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Links de iconos -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+      crossorigin="anonymous"
+    />
     <title>Alcalde Municipal</title>
 </head>
 <body>
 
+<!-- Barra de navegación -->
 <div class="nav">
 
+    
+    <img class= "logo"src="img/logoNuevo.png" alt="" srcset="">
+    <!-- Input de buscar -->
     
     <div>
         <input class="buscar" type="search" name="" id=""  placeholder="Buscar"  > 
         <i class="fa fa-search" > </i>
+        
     </div>
-    
 
     <!-- Barra de navegacion -->
     <nav class="navegacion">
@@ -27,7 +42,7 @@
            <li><a href="">Gobierno</a>
             <ul>
                 <li><a href="alcaldeMunicipal.php">Alcalde Municipal</a></li>
-                <li><a href="">Gobiernos</a></li>
+                <li><a href="">Departamentos</a></li>
                 <li><a href="">Regidores</a></li>
             </ul>
            </li>
@@ -46,7 +61,47 @@
     
   
 </div>
+<!-- Div final de la barra de navegación -->
 
-    
+
+
+<div class="alcaldia">
+    <img src="img/JCMP.jpg" alt="Abog. Juan Carlos Morales Pacheco">
+    <span>Abog. Juan Carlos Morales Pacheco</span> <br>
+    <span>Alcalde Municipal</span><br>
+    <span>2018-2022</span>
+</div> 
+
+<!-- Div final de los datos del alcalde -->
+
+
+<!-- footer -->
+<div class="footer-ciudad">
+    <footer class="footer">
+        <div class="contenedor">
+            <p>Links</p>
+            <div class="links">
+                <a href="https://www.presidencia.gob.hn/">Casa Presidencial</a>
+                <a href="">Noticias</a>
+                <a href="">Agenda</a>
+                <a href="">Contacto</a>
+            </div>
+        </div>
+        <div class="contenedor">
+            <p>Facebook</p>
+            <a href="https://www.facebook.com/siguatepequealcaldia/"><i class="fab fa-facebook"></i></a>
+            
+            
+        </div>
+        <div class="contenedor">
+            <p>Noticias Recientes</p>
+        </div>
+        
+    </footer>
+    <p  >Municipalidad de Siguatepeque &copy 2020. All rights reserved</p>
+</div> 
+<!-- Div final de footer -->
+
+
 </body>
 </html>
