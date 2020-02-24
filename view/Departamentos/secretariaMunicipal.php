ded89d883896d2603e2a166a3571968015a710f0
Luis Dario <luisda13@yahoo.es>
2020-02-24 14:24:18 -0600

    Contenido de view secretaria municipal.

---
+++ view/Departamentos/secretariaMunicipal.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link de icoco -->
+    <link rel="icon" type="image/png" href="/logo.ico" />
+
+    <!-- Hojas de estilo -->
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+
+    <!-- Estilo de la fuente -->
+    <!-- Link de el estilo de las fuentes -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Links de iconos -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+
+    <title>Secretaria Municipal - Municipalidad de Siguatepeque</title>
+</head>
+
+<body>
+    <!-- Barra de navegacion -->
+    <header class="header">
+        <input type="checkbox" id="btn-menu" />
+        <label for="btn-menu">
+            <i class="fa fa-bars" id="btnMenu"></i>
+        </label>
+        <nav class="menu">
+
+
+            <ul>
+
+                <li>
+                    <div class="buscarNav">
+                        <input class="buscarNav" type="search" name="" id="" placeholder="Buscar">
+                        <i class="fa fa-search"> </i>
+                    </div>
+                </li>
+
+
+                <li><a href="/index.php">Inicio</a></li>
+                <li><a href="">Noticias</a></li>
+                <li><a href="">Gobierno</a>
+                    <ul>
+                        <li><a href="alcaldeMunicipal.php">Alcalde Municipal</a></li>
+                        <li><a href="/departamentosMunicipalidad.php">Departamentos </a></li>
+                        <li><a href="">Regidores</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Portal de Transparencia</a></li>
+                <li><a href="">Conoce a Siguatepeque</a>
+                    <ul>
+                        <li><a href="">Números Telefónicos</a></li>
+                        <li><a href="">Lugares Populares</a></li>
+                        <li><a href="">Educación Superior</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Login Linea Base</a></li>
+            </ul>
+
+
+
+        </nav>
+
+        <img class="logo" src="/img/logoNuevo.png" alt="" srcset="">
+        <p class="msg">Municipalidad de Siguatepeque</p>
+
+    </header>
+    <!-- Div final de la barra de navegación -->
+
+    <!-- Input de buscar -->
+    <div class="buscarIndex">
+        <input class="buscar" type="search" name="" id="" placeholder="Buscar">
+        <i class="fa fa-search"> </i>
+    </div>
+
+    <!-- Div de datos del departamento -->
+    <div class="oficina">
+        <p class="texto">SECRETARIA MUNICIPAL</p>
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/10/secretaria-e1573655696973.jpg" alt="" class="imagenDepto">
+
+
+        <ol type="A" class="contenido">
+            <li>Es la Unidad encargada de realizar actos de comunicación, certificación, custodia, notificación, archivo y levantamiento de actas, acuerdos, ordenanzas y resoluciones que la Corporación Municipal resuelva o emita Garantizar la transcripción, notificación y certificación de los acuerdos, ordenanzas y resoluciones de la Corporación Municipal.</li>
+            <li>Comunicar a los miembros corporativos las convocatorias a sesiones, incluyendo la agenda.</li>
+            <li>Archivar, conservar, custodiar los libros de actas, expedientes y demás documentos. </li>
+            <li>Asumir la gestión del RRHH, procurando la armonía en las relaciones obrero patronal. </li>
+            <li>Remitir anualmente copia de actas a la Gobernación Departamental y al Archivo Nacional. </li>
+            <li>Emitir certificaciones de dominio pleno, previo análisis de antecedentes y aprobación por la Corporación Municipal. </li>
+            <li>Auxiliar a las Comisiones nombradas por la Corporación Municipal.</li>
+            <li>Programar y organizar los actos de matrimonio.</li>
+            <li>Velar porque se cumplan los requisitos de Ley y se conformen los expedientes respectivos.</li>
+            <li>Recomendar sobre asuntos de su competencia. </li>
+            <li>Custodia y administración de fondos de caja chica.</li>
+            <li>Realizar los trámites pertinentes para la autorización de libros contables y de actas conforme a ley. </li>
+            <li>Otras que la Corporación le designe. </li>
+            <li>Concurrir a las sesiones de la Corporación Municipal y levantar las actas correspondientes </li>
+        </ol>
+
+    </div>
+
+    <!-- Footer -->
+
+    <footer class="footer">
+        <div class="contenedor">
+            <p>Links</p>
+            <div class="links">
+                <a href="https://www.presidencia.gob.hn/">Casa Presidencial</a>
+                <a href="">Noticias</a>
+                <a href="">Agenda</a>
+                <a href="">Contacto</a>
+            </div>
+        </div>
+        <div class="contenedor">
+            <p>Facebook</p>
+            <a href="https://www.facebook.com/siguatepequealcaldia/"><i class="fab fa-facebook"></i></a>
+
+
+        </div>
+        <div class="contenedor">
+            <p>Noticias Recientes</p>
+        </div>
+        <p>Municipalidad de Siguatepeque &copy 2020. All rights reserved</p>
+
+    </footer>
+
+
+</body>
+
+</html>
