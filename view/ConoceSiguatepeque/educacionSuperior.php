50e60a6e584a6805cb4aa2d7292d672d2ef1fb01
Luis Dario <luisda13@yahoo.es>
2020-02-28 13:53:13 -0600

    Direccionar las view en su respectiva ruta.

--- view/ConoceSiguatepeque/educacionSuperior.php
+++ view/ConoceSiguatepeque/educacionSuperior.php
@@ -72,27 +72,39 @@
     <div class="educacionSuperior">
       <div class="contenedor">
         <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/uthlogo-e1573588651204.png" alt="" class="imagen">
-        <p>Universidad Técnologica de Honduras</p>
+        <a href="/view/EducacionSuperior/uth.php">
+          <p>Universidad Técnologica de Honduras</p>
+        </a>
       </div>
       <div class="contenedor">
         <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/umh1-e1573588542345-740x460.png" alt="" class="imagen">
-        <p>Universidad Metropolitana de Honduras</p>
+        <a href="/view/EducacionSuperior/umh.php">
+          <p>Universidad Metropolitana de Honduras</p>
+        </a>
       </div>
       <div class="contenedor">
         <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/LOGOUNACIFOR-e1573590546926.png" alt="" class="imagen">
-        <p>UNACIFOR Siguatepeque Honduras</p>
+        <a href="/view/EducacionSuperior/unacifor.php">
+          <p>UNACIFOR Siguatepeque Honduras</p>
+        </a>
       </div>
       <div class="contenedor">
         <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/LOUNFM-740x460.jpg" alt="" class="imagen">
-        <p>Universidad Pedagógica Francisco Morazán</p>
+        <a href="/view/EducacionSuperior/upnfm.php">
+          <p>Universidad Pedagógica Francisco Morazán</p>
+        </a>
       </div>
       <div class="contenedor">
         <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/logo-unah-740x460.jpg" alt="" class="imagen">
-        <p>UNAH-CRAED Siguatepeque Honduras</p>
+        <a href="/view/EducacionSuperior/unah.php">
+          <p>UNAH-CRAED Siguatepeque Honduras</p>
+        </a>
       </div>
       <div class="contenedor">
         <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/logounicah-e1573591204254-740x460.png" alt="" class="imagen">
-        <p>Universidad Católica de Honduras</p>
+        <a href="/view/EducacionSuperior/unicah.php">
+          <p>Universidad Católica de Honduras</p>
+        </a>
       </div>
     </div>
 
