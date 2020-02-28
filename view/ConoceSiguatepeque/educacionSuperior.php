903aa9bd8add299aa7d733798adef7101ea8e177
Luis Dario <luisda13@yahoo.es>
2020-02-28 15:42:58 -0600

    Contenido de la view de universidad metropolitana de honduras.

--- view/ConoceSiguatepeque/educacionSuperior.php
+++ view/ConoceSiguatepeque/educacionSuperior.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Link de icoco -->
-  <link rel="icon" type="image/png" href="logo.ico" />
+  <link rel="icon" type="image/png" href="/logo.ico" />
 
   <!-- Link de las hojas de estilos -->
   <link rel="stylesheet" href="/css/normalize.css">
