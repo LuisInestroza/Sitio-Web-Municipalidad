e45690264ba066c80b0e8174a62abc68a1930110
Luis Dario <luisda13@yahoo.es>
2020-03-26 00:37:12 -0600

    Cambio de la etiqueta tittle en cada view

--- view/Departamentos/catastroMunicipal.php
+++ view/Departamentos/catastroMunicipal.php
@@ -1,5 +1,8 @@
 <!-- Header -->
-<?php require_once '../layouts/header.php'; ?>
+<?php 
+  $tittle ="Catastro Municipal - Municipalidad de Siguatepeque";
+  require_once '../layouts/header.php'; 
+?>
 
 
 <!-- Contenido de catastro -->
