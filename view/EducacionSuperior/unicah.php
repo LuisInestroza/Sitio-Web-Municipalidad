31fac682ea7d8afcc72f3e343c12263f1cffd284
Luis Dario <luisda13@yahoo.es>
2020-03-29 01:32:36 -0600

    Cambio en el posicionamiento response del aside

--- view/EducacionSuperior/unicah.php
+++ view/EducacionSuperior/unicah.php
@@ -5,48 +5,60 @@
 ?>
 
 
-<!-- Aside -->
-
-<aside>
-  <!-- Contenido de redes sociales -->
-  <div class="contenedorContactos">
-    <p>Contactos</p>
-    <div class="links">
-      <ul class="icons">
-        <li>
-          <a href="https://www.unicah.edu" class="fa fa-globe-americas"></a>
-        </li>
-        <li>
-          <a href="https://es-la.facebook.com/UNICAHCJS/  " class="fab fa-facebook"></a>
-        </li>
-        <li>
-          <a href="https://twitter.com/unicahjs" class="fab fa-twitter"></a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/unicahcjs/" class="fab fa-instagram"></a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <p>Ubicación</p>
-  <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15443.357535311456!2d-87.83542913569059!3d14.608223883254785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f65950c2a62de1b%3A0x35ced69748e8107c!2sUniversidad%20Cat%C3%B3lica%20de%20Honduras!5e0!3m2!1ses!2shn!4v1583939606494!5m2!1ses!2shn"
-    width="350" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
-    tabindex="0"></iframe>
-
-
-</aside>
-
 <!-- Contenido -->
 <div class="universidad">
-  <p class="texto">Universidad Católica de Honduras</p>
-  <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/logounicah-e1573591204254.png" alt=""
-    class="imagenUniversidad">
-  <p class="contenido">
-    Somos una institución educativa católica confesional con altos valores morales y espirituales de educación
-    Universitaria.
-  </p>
+  <!-- Aside -->
+  <aside>
+    <!-- Contenido de redes sociales -->
+    <div class="contenedorContactos">
+      <p>Contactos</p>
+      <div class="links">
+        <ul class="icons">
+          <li>
+            <a href="https://www.unicah.edu" class="fa fa-globe-americas"></a>
+          </li>
+          <li>
+            <a href="https://es-la.facebook.com/UNICAHCJS/  " class="fab fa-facebook"></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/unicahjs" class="fab fa-twitter"></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/unicahcjs/" class="fab fa-instagram"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <p>Ubicación</p>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15443.357535311456!2d-87.83542913569059!3d14.608223883254785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f65950c2a62de1b%3A0x35ced69748e8107c!2sUniversidad%20Cat%C3%B3lica%20de%20Honduras!5e0!3m2!1ses!2shn!4v1583939606494!5m2!1ses!2shn"
+      frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+  </aside>
+
+  <div>
+    <p class="texto">Universidad Católica de Honduras</p>
+    <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/logounicah-e1573591204254.png" alt=""
+      class="imagenUniversidad">
+    <p class="contenido">
+      Somos una institución educativa católica confesional con altos valores morales y espirituales de educación
+      Universitaria.
+    </p>
+
+  </div>
+
+
+
 </div>
+
+
+
+
+
+
+
+
+
 
 
 
