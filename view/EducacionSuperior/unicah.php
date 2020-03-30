792f777843713f5a4ccf90306ec1846aa7cd517e
Luis Dario <luisda13@yahoo.es>
2020-03-29 22:25:44 -0600

    Mejorar cambios en las view de educación superior.

--- view/EducacionSuperior/unicah.php
+++ view/EducacionSuperior/unicah.php
@@ -29,15 +29,17 @@
         </ul>
       </div>
     </div>
-    <p>Ubicación</p>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15443.357535311456!2d-87.83542913569059!3d14.608223883254785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f65950c2a62de1b%3A0x35ced69748e8107c!2sUniversidad%20Cat%C3%B3lica%20de%20Honduras!5e0!3m2!1ses!2shn!4v1583939606494!5m2!1ses!2shn"></iframe>
+    <div class="iframe">
+      <p>Ubicación</p>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15443.357535311456!2d-87.83542913569059!3d14.608223883254785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f65950c2a62de1b%3A0x35ced69748e8107c!2sUniversidad%20Cat%C3%B3lica%20de%20Honduras!5e0!3m2!1ses!2shn!4v1583939606494!5m2!1ses!2shn"></iframe>
+
+    </div>
   </aside>
 
   <div>
     <p class="texto">Universidad Católica de Honduras</p>
-    <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/logounicah-e1573591204254.png" alt=""
-      class="imagenUniversidad">
+    <img src="/img/logosUniversidades/logoUnicah.png" alt="" class="imagenUniversidad">
     <p class="contenido">
       Somos una institución educativa católica confesional con altos valores morales y espirituales de educación
       Universitaria.
