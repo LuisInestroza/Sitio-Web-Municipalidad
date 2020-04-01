7c1aa9413b22e2c83607e8c9711503fb92af84d9
Luis Dario <luisda13@yahoo.es>
2020-03-31 23:44:23 -0600

    Contenido de view de umh.php

--- view/EducacionSuperior/umh.php
+++ view/EducacionSuperior/umh.php
@@ -1,26 +1,89 @@
 <!-- Header -->
 <?php
-  $tittle ="Universidad Metropolitana de Honduras - Municipalidad de Siguatepeque";
-  require_once '../layouts/header.php'; ?>
+$tittle = "Universidad Metropolitana de Honduras - Municipalidad de Siguatepeque";
+require_once '../layouts/header.php'; ?>
 
-<!-- Contenido -->
+
 <div class="universidad">
-  <p class="texto">Universidad Metropolitana de Honduras</p>
-  <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/umh1-e1573588542345-740x460.png" alt=""
-    class="imagenUniversidad">
+  <aside>
+    <!-- Contenedor de redes -->
+    <div class="contenedorContactos">
+      <p>Contactos</p>
+      <div class="links">
+        <ul class="icons">
+          <li><a href="https://www.unimetro.edu.hn/" class="fas fa-globe"></a></li>
+          <li><a href="https://www.facebook.com/UniversidadMetropolitanaDeHondurasSiguatepeque/"
+              class="fab fa-facebook"></a></li>
+          <li><a href="https://twitter.com/UMHHonduras" class="fab fa-twitter"></a></li>
+          <li><a href="https://www.instagram.com/umh.unimetro/?hl=es-la" class="fab fa-instagram"></a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- Contenedor Ubicación -->
+    <div class="iframe">
+      <p>Ubicación</p>
 
-  <p class="contenido">
-    Contribuir al desarrollo social, económico, tecnológico y político del país,
-    mediante la formación de recursos humanos de calidad y competitividad, capaces
-    de transformar las estructuras sociales, económicas y políticas. Formar generaciones
-    de profesionales, con valores y actitudes positivas acordes a su condición humana y las
-    exigencias de ética y moral de la sociedad.
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.2647724071446!2d-87.84893077083915!3d14.59570879936285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f65958383fca171%3A0xe1d723b1168f2783!2sInstituto%20de%20Ciencia%20y%20Tecnolog%C3%ADa%20Omega!5e0!3m2!1ses!2shn!4v1585717794445!5m2!1ses!2shn"></iframe>
+      Instalaciones del Instituto Omega
+    </div>
 
-  </p>
-  <p class="contenido">
+    <!-- Carreras de la universidad -->
+    <div class="carreras">
+      <p>Carreras</p>
+      <ul>
+        <li><a
+            href="https://1291ba06-3447-538f-b437-710f5712feb1.filesusr.com/ugd/de548f_ba8d0698bf124828bd0909c6699db950.pdf">
+            Ingeniería de Negocios</a></li>
+        <li><a
+            href="https://1291ba06-3447-538f-b437-710f5712feb1.filesusr.com/ugd/de548f_ae3bf242bbfb4abd91dde998e69e20c6.pdf">
+            Ingeniería en Informática</a></li>
+        <li><a
+            href="https://1291ba06-3447-538f-b437-710f5712feb1.filesusr.com/ugd/de548f_f3d52499717947aca5029513e200772f.pdf">
+            Gerencia y Desarrollo Social</a></li>
+        <li><a
+            href="https://1291ba06-3447-538f-b437-710f5712feb1.filesusr.com/ugd/de548f_7e591a6771bd47a0936cdcf607556a2a.pdf">
+            Gestión y Contaduría Empresarial</a></li>
+        <li><a
+            href="https://1291ba06-3447-538f-b437-710f5712feb1.filesusr.com/ugd/de548f_93ebb6c17d5a4819a6e560b6ab2cf0ea.pdf">
+            Marketing y Negocios Internacionales</a></li>
+        <li><a
+            href="https://1291ba06-3447-538f-b437-710f5712feb1.filesusr.com/ugd/de548f_b3b1a5bde73c48238e26d8bcd04625ac.pdf">
+            Derecho</a></li>
 
-  </p>
+      </ul>
+    </div>
+  </aside>
+
+  <div>
+    <p class="texto">Universidad Metropolitana de Honduras</p>
+    <img src="/img/logosUniversidades/logoUMH.png" alt="" class="imagenUniversidad">
+    <p class="contenido">
+      La Universidad Metropolitana de Honduras es una institución de educación superior fundada en el año 2002, la cual
+      tiene como fin; ser una institución basada en la responsabilidad social reflejándonos en los precios, la calidad y
+      todos los beneficios que ofrece al pueblo hondureño al cual sirve. Esto con el propósito de posicionarse como una
+      organización que se preocupa por el desarrollo social y educativo de las personas que aspiran a un título
+      universitario de excelencia. <br><br>
+
+      <b>Misión</b> <br>
+      Contribuir al desarrollo social, económico, tecnológico y político del país, mediante la formación de recursos
+      humanos de calidad y competitividad, capaces de transformar las estructuras sociales, económicas y políticas.
+      Formar generaciones de profesionales, con valores y actitudes positivas acordes a su condición humana y las
+      exigencias de ética y moral de la sociedad. <br><br>
+
+      <b>Visión</b> <br>
+      Ser centro de enseñanza de Educación Superior que brinde SERVICIOS EDUCATIVOS de calidad, con proyección
+      internacional en los diferentes campos del conocimiento humano. Ser una universidad de avanzada en las técnicas y
+      metodológicas relacionadas con la creación y transmisión de la ciencia, las artes y la cultura, conformando un
+      sistema de aprendizaje que vincule o académico con las necesidades y la problemática nacional, regional y local a
+      fin de contribuir a su solución. <br><br>
+
+
+    </p>
+  </div>
 </div>
+
+
 
 <!-- footer -->
 <?php require_once '../layouts/footer.php'; ?>
