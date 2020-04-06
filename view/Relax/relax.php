f5b295b8970b0e923f0fea387b09959e1a115642
Luis Dario <43349624+LuisInestroza@users.noreply.github.com>
2020-04-05 21:52:34 -0600

    Cambios.

--- view/Relax/relax.php
+++ view/Relax/relax.php
@@ -11,70 +11,81 @@
   <p class="texto">Lugares Populares</p>
   <div class="lugares">
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/eliben2-740x460.jpg" alt=""
-        class="imagen">
+
 
       <a href="/view/Relax/centroEliben.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/eliben2-740x460.jpg" alt=""
+          class="imagen">
         <p>Centro Turistico Eliben</p>
       </a>
 
 
     </div>
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/lakes1-1-740x448.jpg" alt=""
-        class="imagen">
+
       <a href="/view/Relax/balnearioAguasCalientes.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/lakes1-1-740x448.jpg" alt=""
+          class="imagen">
         <p>Balneario Lake’s Aguas calientes</p>
       </a>
     </div>
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/waterislan2-1-740x460.jpg" alt=""
-        class="imagen">
+
 
       <a href="/view/Relax/balnearioWaterIsland.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/waterislan2-1-740x460.jpg" alt=""
+          class="imagen">
         <p>Balneario Water Island</p>
       </a>
     </div>
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/10/plaza-sanpedro2.jpg" alt=""
-        class="imagen">
+
       <a href="/view/Relax/plazaSanPedro.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/10/plaza-sanpedro2.jpg" alt=""
+          class="imagen">
         <p>Plaza San Pedro Siguatepeque</p>
       </a>
     </div>
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/10/calanterique2-717x460.jpeg" alt="">
+
       <a href="/view/Relax/bosqueCalanterique.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/10/calanterique2-717x460.jpeg" alt=""
+          class="imagen">
         <p>Bosque Calenterique</p>
       </a>
     </div>
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/siguaextreme-740x460.jpg" alt=""
-        class="imagen">
+
       <a href="/view/Relax/centroSiguaxtreme.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/siguaextreme-740x460.jpg" alt=""
+          class="imagen">
         <p>CentroTurístico Siguaxtreme</p>
       </a>
     </div>
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/aventura4-740x460.jpg" alt=""
-        class="imagen">
+
       <a href="/view/Relax/parkPlaceCenter.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/aventura4-740x460.jpg" alt=""
+          class="imagen">
         <p>Park Place Hotel & Cinema</p>
       </a>
     </div>
     <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/casagrande2-740x460.jpg" alt=""
-        class="imagen">
+
       <a href="/view/Relax/casaGrande.php">
+        <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/casagrande2-740x460.jpg" alt=""
+          class="imagen">
+
         <p>La Casa Grande Siguatepeque</p>
       </a>
     </div>
     <div class="contenedor">
-      <img
-        src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/71403647_2422253198049177_4278101350270631936_o-740x460.jpg"
-        alt="" class="imagen">
+
       <a href="/view/Relax/granjaD'elia.php">
-        <p>Centro Turistico Granja D’elia</p>
+        <img
+          src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/11/71403647_2422253198049177_4278101350270631936_o-740x460.jpg"
+          alt="" class="imagen">
+        <p>Centro Turístico Granja D’elia</p>
       </a>
     </div>
 
