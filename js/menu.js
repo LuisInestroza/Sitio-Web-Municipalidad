69b2de72fde93f3fdd00fb811bca35ce4ecefad2
Luis Dario <43349624+LuisInestroza@users.noreply.github.com>
2020-04-18 02:01:19 -0600

    Mejoras en la navegación

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 // Codigo para dar funcinamiento a los submenus en modo responsive
-$(document).ready(function() {
+$(document).ready(function () {
   // Captar el nombre de la clase de la etiqueta <li></li>
-  $(".submenu > a").click(function(e) {
+  $(".submenu > a").click(function (e) {
     e.preventDefault();
     $(this)
       // mostrar los elementos de la etiqueta <li></li>
--- view/layouts/header.php
+++ view/layouts/header.php
@@ -12,18 +12,8 @@
   <link rel="stylesheet" href="/css/normalize.css">
   <link rel="stylesheet" href="/css/style.css">
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="/css/bootstrap-grid.css">
-  <link rel="stylesheet" href="/css/bootstrap-grid.css.map">
-  <link rel="stylesheet" href="/css/bootstrap-grid.min.css">
-  <link rel="stylesheet" href="/css/bootstrap-grid.min.css.map">
-  <link rel="stylesheet" href="/css/bootstrap-reboot.css">
-  <link rel="stylesheet" href="/css/bootstrap-reboot.css.map">
-  <link rel="stylesheet" href="/css/bootstrap-reboot.min.css">
-  <link rel="stylesheet" href="/css/bootstrap-reboot.min.css.map">
+
   <link rel="stylesheet" href="/css/bootstrap.css">
-  <link rel="stylesheet" href="/css/bootstrap.css.map">
-  <link rel="stylesheet" href="/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/css/bootstrap.min.css.map">
 
   <!-- Estilo del slider de noticias -->
 
@@ -44,20 +34,24 @@
   <!-- Boton de desplaxar hacia arriba -->
   <span class="arriba"><i class="fa fa-angle-up"></i></span>
   <!-- Barra de navegacion -->
-  <header class="header">
-    <img class=" logo" src="/img/logoMuni.png" alt="" srcset="">
-    <!-- Llamar a la variable -->
-    <p class="msg">Municipalidad de Siguatepeque <br>
-      Gobierno Municipal (2018-2020)</p>
-    <!-- Final -->
-    <input type="checkbox" id="btn-menu" />
-    <label for="btn-menu"><i class="fa fa-bars" id="btnMenu"></i></label>
 
-    <nav class="menu">
+  <header class="header">
+    <img class="logo" src="/img/logoMuni.png" alt="" srcset="">
+    <p class="msg">
+      Municipalidad de Siguatepeque
+      <br>
+      Gobierno Municipal (2018-2020)
+    </p>
+
+    <input type="checkbox" id="btnMenu">
+    <label for="btnMenu"><i class="fas fa-bars"></i></label>
+
+    <nav class=menu>
+
       <ul>
         <li><a href="/index.php"><b>Inicio</b></a></li>
         <li><a href=""><b>Noticias</b></a></li>
-        <li class="submenu"><a href="#"><b>Gobierno</b><i class="fa fa-angle-down "></i></a>
+        <li class="submenu"><a href="#"><b>Gobierno</b><i class="fa fa-angle-down flechaUno"></i></a>
           <ul class="children">
             <li><a href="/view/Gobierno/alcaldeMunicipal.php">Alcalde Municipal</a></li>
             <li><a href="/view/Gobierno/departamentosMunicipalidad.php">Departamentos </a></li>
@@ -65,7 +59,7 @@
           </ul>
         </li>
         <li><a href=""><b>Portal de Transparencia</b></a></li>
-        <li class="submenu"><a href="#"><b>Conoce Siguatepeque</b><i class="fa fa-angle-down "></i></a>
+        <li class="submenu"><a href="#"><b>Conoce Siguatepeque</b><i class="fa fa-angle-down flechaDos"></i></a>
           <ul class="children">
             <li><a href="/view/ConoceSiguatepeque/contactos.php">Números Telefónicos</a></li>
             <li><a href="/view/Relax/relax.php">Lugares Populares</a></li>
@@ -74,12 +68,23 @@
         </li>
         <li><a href=""><b>Login Linea Base</b></a></li>
 
-        <li class="libuscar">
-          <i class="fa fa-search"></i>
-          <input type="search" placeholder="Buscar..">
-        </li>
+
       </ul>
 
+
     </nav>
+
   </header>
+
+
+  <!-- 
+  <header class="header">
+    
+
+
+    <nav class="menu">
+
+
+    </nav>
+  </header> -->
   <!-- Div final de la barra de navegación -->
