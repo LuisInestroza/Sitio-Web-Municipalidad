6f8d91a29900e7edb90bed49dc263e06414e7f38
Luis Dario <43349624+LuisInestroza@users.noreply.github.com>
2020-05-15 20:23:12 -0600

    Quitar el fondo blanco de la imagene de los logos de las universidadews

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
   // Titulo de la pagina 
   $tittle = "Municipalidad de Siguatepeque";
   // Iniciar valores de la pagina
-
   require_once 'view/layouts/header.php';
   require_once 'view/home.php';
   require_once 'view/layouts/footer.php';
-
 
 ?>
