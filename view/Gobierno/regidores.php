40391fe98c0221c1cae5e569e0db0b51c4532e2d
Luis Dario <43349624+LuisInestroza@users.noreply.github.com>
2020-10-24 03:43:59 -0600

    Correcion de errores.

--- view/Gobierno/regidores.php
+++ view/Gobierno/regidores.php
@@ -4,132 +4,120 @@
   require_once '../layouts/header.php'; 
 ?>
 <div class="tittle">
-  <h1>REGIDORES 2018-2022</h1>
+    <h1>REGIDORES 2018-2022</h1>
 </div>
 <div class="lista-regidores">
 
 
-  <div class="regidores">
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/santiagoventura-370x370.jpg" alt=""
-        class="imagenRegidor">
+    <div class="regidores">
+        <div class="contenedor">
+            <img src="/img/regidores/santiagoventura.jpg" alt="" class="imagenRegidor">
 
-      <div class="informacion-regidor">
-        <p class="regidor">PRIMER REGIDOR</p>
-        <p class="nombre-regidor">Santiago Ventura</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
-      </div>
+            <div class="informacion-regidor">
+                <p class="regidor">PRIMER REGIDOR</p>
+                <p class="nombre-regidor">Santiago Ventura</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/aslycruz-2-370x370.jpg" alt=""
-        class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">SEGUNDO REGIDOR</p>
-        <p class="nombre-regidor">Ashley Cruz</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
-      </div>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/aslycruz.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">SEGUNDO REGIDOR</p>
+                <p class="nombre-regidor">Ashley Cruz</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/telmarecarte-e1571332656124.jpg"
-        alt="" class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">TERCER REGIDOR</p>
-        <p class="nombre-regidor">Telma Recarte</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
-      </div>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/telmarecarte.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">TERCER REGIDOR</p>
+                <p class="nombre-regidor">Telma Recarte</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/alquiles-e1573655098931-370x308.jpg"
-        alt="" class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">CUARTO REGIDOR</p>
-        <p class="nombre-regidor">Efrain Perira</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/efrainperira.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">CUARTO REGIDOR</p>
+                <p class="nombre-regidor">Efrain Perira</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
 
-      </div>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/david-e1573654064790.jpg" alt=""
-        class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">QUINTO REGIDOR</p>
-        <p class="nombre-regidor">David Valenzuela</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
-      </div>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/davidvalenzuela.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">QUINTO REGIDOR</p>
+                <p class="nombre-regidor">David Valenzuela</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img
-        src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/carlos-luis-2-e1573654166258-370x370.jpg"
-        alt="" class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">SEXTO REGIDOR</p>
-        <p class="nombre-regidor">Carlos Reyes</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/carlos-luis.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">SEXTO REGIDOR</p>
+                <p class="nombre-regidor">Carlos Reyes</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
 
-      </div>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/descarga-1.png" alt=""
-        class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">SÉPTIMO REGIDOR</p>
-        <p class="nombre-regidor">Erika Molina</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/descarga.png" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">SÉPTIMO REGIDOR</p>
+                <p class="nombre-regidor">Erika Molina</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
 
-      </div>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2016/01/napoleon-370x320.jpg" alt=""
-        class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">OCTAVO REGIDOR</p>
-        <p class="nombre-regidor">Napoleon Mendoza</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/napoleon.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">OCTAVO REGIDOR</p>
+                <p class="nombre-regidor">Napoleon Mendoza</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
 
-      </div>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/10/hectorchavez-e1573654715274.jpg"
-        alt="" class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">NOVENO REGIDOR</p>
-        <p class="nombre-regidor">Hector Chavez</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/hectorchavez.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">NOVENO REGIDOR</p>
+                <p class="nombre-regidor">Hector Chavez</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
 
-      </div>
+            </div>
 
-    </div>
-    <div class="contenedor">
-      <img
-        src="http://municipalidaddesiguatepeque.com/wp-content/uploads/2019/10/anibal-discua-2-e1573654411438-370x370.jpg"
-        alt="" class="imagenRegidor">
-      <div class="informacion-regidor">
-        <p class="regidor">DÉCIMO REGIDOR</p>
-        <p class="nombre-regidor">Anibal Discua</p>
-        <p class="phone-regidor">(123) 456-7890</p>
-        <p class="email-regidor">clerk@citygov.com</p>
-      </div>
+        </div>
+        <div class="contenedor">
+            <img src="/img/regidores/anibal-discua.jpg" alt="" class="imagenRegidor">
+            <div class="informacion-regidor">
+                <p class="regidor">DÉCIMO REGIDOR</p>
+                <p class="nombre-regidor">Anibal Discua</p>
+                <p class="phone-regidor">(123) 456-7890</p>
+                <p class="email-regidor">clerk@citygov.com</p>
+            </div>
+
+        </div>
 
     </div>
-
-  </div>
 
 </div>
 
